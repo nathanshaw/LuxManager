e3aabe1db837bfdb34b062d47fd6f79a9619a1d8
nathanshaw <nathanshawsemail@gmail.com>
2020-06-29 17:29:35 -0700

    added support for the 6030's

--- LuxManager.h
+++ LuxManager.h
@@ -31,106 +31,109 @@
 
 class LuxManager {
     // initalises its own lux sensors and then handles the readings
-  public:
-    LuxManager(long minrt, long maxrt);
-    void linkNeoGroup(NeoGroup * n);
-    void add6030Sensors();
-    void add7700Sensors();
-	void addSensorTcaIdx(String _name, int tca);
+    public:
+        LuxManager(long minrt, long maxrt);
+        void linkNeoGroup(NeoGroup * n);
+        void add6030Sensors(float gain, int _int);
+        void add7700Sensors();
+        void addSensorTcaIdx(String _name, int tca);
 
-    double getLux() {
-      return global_lux;
-    };
-    void startTCA7700Sensors(byte gain, byte integration);
+        double getLux() {
+            return global_lux;
+        };
+        void startTCA7700Sensors(byte gain, byte integration);
 
-    // to help multiple lux managers coordinate
-    double forceLuxReading();
-    void calibrate(long len, bool first_time);
+        // to help multiple lux managers coordinate
+        double forceLuxReading();
+        void calibrate(long len, bool first_time);
 
-    bool update();
-    void resetMinMax();
-    double min_reading = 9999.9;
-    double max_reading = 0.0;
+        bool update();
+        void resetMinMax();
+        double min_reading = 9999.9;
+        double max_reading = 0.0;
 
-    double getAvgLux();
-    void   resetAvgLux();
+        double getAvgLux();
+        void   resetAvgLux();
 
-    String getName(int i) {return names[i];};
+        String getName(int i) {return names[i];};
 
-    // brightness and brightness scalers
-    double getBrightnessScaler();
-    double getBrightnessScalerAvg();
-    void resetBrightnessScalerAvg();
-    bool getExtremeLux() {return extreme_lux;};
+        // brightness and brightness scalers
+        double getBrightnessScaler();
+        double getBrightnessScalerAvg();
+        void resetBrightnessScalerAvg();
+        bool getExtremeLux() {return extreme_lux;};
 
-    double brightness_scaler = 0.0;
-    double brightness_scaler_avg = 0.0;
-    double lux[MAX_LUX_SENSORS];
-    double global_lux;
+        double brightness_scaler = 0.0;
+        double brightness_scaler_avg = 0.0;
+        double lux[MAX_LUX_SENSORS];
+        double global_lux;
 
-  private:
-    Adafruit_VEML7700       sensors_7700[MAX_LUX_SENSORS];
-    SparkFun_Ambient_Light  sensors_6030[MAX_LUX_SENSORS] = {
-        SparkFun_Ambient_Light(V6030_ADDR1), SparkFun_Ambient_Light(V6030_ADDR2)};
+    private:
+        Adafruit_VEML7700       sensors_7700[MAX_LUX_SENSORS];
+        SparkFun_Ambient_Light  sensors_6030[MAX_LUX_SENSORS] = {
+            SparkFun_Ambient_Light(V6030_ADDR1), SparkFun_Ambient_Light(V6030_ADDR2)};
 
-    uint8_t mode = TAKE_HIGHEST_LUX;
-    uint8_t num_sensors = 0;
-    bool sensor_active[MAX_LUX_SENSORS];
+        uint8_t mode = TAKE_HIGHEST_LUX;
+        uint8_t num_sensors = 0;
+        uint8_t num_6030_sensors = 0;
+        uint8_t num_7700_sensors = 0;
+        bool sensor_active[MAX_LUX_SENSORS];
 
-    int tca_addr[MAX_LUX_SENSORS];
+        int tca_addr[MAX_LUX_SENSORS];
 
-    NeoGroup *neos[MAX_NEO_GROUP];
-    uint8_t num_neo_groups = 0;
-    String names[MAX_LUX_SENSORS];
+        NeoGroup *neos[MAX_NEO_GROUP];
+        uint8_t num_neo_groups = 0;
+        String names[MAX_LUX_SENSORS];
 
-    void updateMinMax();
+        void updateMinMax();
 
-    double past_readings[MAX_LUX_SENSORS][10];
+        double past_readings[MAX_LUX_SENSORS][10];
 
-    void readLux();
+        void readLux();
 
-    unsigned long min_reading_time;
-    unsigned long max_reading_time;
+        unsigned long min_reading_time;
+        unsigned long max_reading_time;
 
-    elapsedMillis last_reading;
-    long polling_rate;
+        elapsedMillis last_reading;
+        long polling_rate;
 
-    // for datalogging and such
-    double lux_total;
-    double lux_readings;
+        // for datalogging and such
+        double lux_total;
+        double lux_readings;
 
-    // for brightness
-    double brightness_scaler_total;
-    uint32_t num_brightness_scaler_vals;
+        // for brightness
+        double brightness_scaler_total;
+        uint32_t num_brightness_scaler_vals;
 
-    double calculateBrightnessScaler();
+        double calculateBrightnessScaler();
 
-    double read();
-    bool extreme_lux;
+        double read();
+        bool extreme_lux;
 };
 
 LuxManager::LuxManager(long minrt, long maxrt){
-      min_reading_time = minrt;
-      max_reading_time = maxrt;
+    min_reading_time = minrt;
+    max_reading_time = maxrt;
 }
 
 //////////////////////////// lux and stuff /////////////////////////
 
 
 void LuxManager::addSensorTcaIdx(String _name, int tca){
-  names[num_sensors] = _name;
-  tca_addr[num_sensors] = tca;
-  sensors_7700[num_sensors] = Adafruit_VEML7700();
-  sensor_active[num_sensors] = false;// not active until startSensors() is called
-  num_sensors++;
+    names[num_sensors] = _name;
+    tca_addr[num_sensors] = tca;
+    sensors_7700[num_sensors] = Adafruit_VEML7700();
+    sensor_active[num_sensors] = false;// not active until startSensors() is called
+    num_sensors++;
+    num_7700_sensors++;
 }
 /*
 // for adding a sensor which is not based on tca
 void LuxManager::addSensorI2CAddr(String _name, int addr){
-  names[num_sensors] = _name;
-  num_sensors] = Adafruit_VEML7700();
-  sensor_active[num_sensors] = false;// not active until startSensors() is called
-  num_sensors++;
+names[num_sensors] = _name;
+num_sensors] = Adafruit_VEML7700();
+sensor_active[num_sensors] = false;// not active until startSensors() is called
+num_sensors++;
 }
 */
 
@@ -138,73 +141,98 @@ void LuxManager::add7700Sensors() {
     Serial.println("TODO");
 }
 
-void LuxManager::add6030Sensors() {
+void LuxManager::add6030Sensors(float gain, int _int) {
+    Wire.begin();
+    Serial.println("Adding VEML6030 Lux Sensors");
     names[num_sensors] = "Front";
     // sensors_6030[num_sensors] = SparkFun_Ambient_Light(V6030_ADDR1);
-    sensor_active[num_sensors] = false;
-    num_sensors++;
+    if (sensors_6030[num_sensors].begin()){
+        Serial.println("added first VEML6030 sensor");
+        sensors_6030[num_sensors].setGain(gain);
+        sensors_6030[num_sensors].setIntegTime(_int);
+        Serial.println("configured first VEML6030 sensor");
+        sensor_active[num_sensors] = true;
+        num_sensors++;
+        num_6030_sensors++;
+    }
+    else {
+        Serial.println("WARNING - TROUBLE STARTING VEML6030 SENSOR");
+    }
 
     names[num_sensors] = "Rear";
     // sensors_6030[num_sensors] = SparkFun_Ambient_Light(V6030_ADDR2);
-    sensor_active[num_sensors] = false;
-    num_sensors++;
+    if (sensors_6030[num_sensors].begin()){
+        Serial.println("added second VEML6030 sensor");
+        sensors_6030[num_sensors].setGain(gain);
+        sensors_6030[num_sensors].setIntegTime(_int);
+        Serial.println("configured second VEML6030 sensor");
+        sensor_active[num_sensors] = true;
+
+        num_sensors++;
+        num_6030_sensors++;
+    }
+    else {
+        Serial.println("WARNING - TROUBLE STARTING VEML6030 SENSOR");
+    }
+    Serial.print(num_sensors);
+    Serial.println(" sensors were added correctly");
 }
 
 // void LuxManager::findSensor() {
-    // search for veml7700 sensor
-    // search for veml7700 sensor via TCA
-    // search for veml6030 sensor
+// search for veml7700 sensor
+// search for veml7700 sensor via TCA
+// search for veml6030 sensor
 
-    // determine what side of the PCB the sensor is on (according to neogroups)
-    // link the lux sensors to the correct neogroups if that is needed/wanted
+// determine what side of the PCB the sensor is on (according to neogroups)
+// link the lux sensors to the correct neogroups if that is needed/wanted
 // }
 
 void LuxManager::linkNeoGroup(NeoGroup * n){
-  neos[num_neo_groups] = n;
-  num_neo_groups++;
+    neos[num_neo_groups] = n;
+    num_neo_groups++;
 }
 
 double LuxManager::getAvgLux() {
-  return lux_total / (double) lux_readings;
+    return lux_total / (double) lux_readings;
 }
 
 void LuxManager::resetAvgLux() {
-  lux_total = 0;
-  lux_readings = 0;
-  dprintln(PRINT_LUX_DEBUG, "reset lux_total and lux_readings");
+    lux_total = 0;
+    lux_readings = 0;
+    dprintln(PRINT_LUX_DEBUG, "reset lux_total and lux_readings");
 }
 
 void tcaselect(uint8_t i) {
-  if (i > 7) return;
-  Wire.beginTransmission(TCAADDR);
-  Wire.write(1 << i);
-  Wire.endTransmission();
+    if (i > 7) return;
+    Wire.beginTransmission(TCAADDR);
+    Wire.write(1 << i);
+    Wire.endTransmission();
 }
 
 void LuxManager::startTCA7700Sensors(byte gain, byte integration) {
     Wire.begin();
     delay(500);
     for (int i = 0; i < num_sensors; i++){
-      Serial.print("\nattempting to start lux sensor ");
-      Serial.println(names[i]);
-      if (tca_addr[i] > -1) {
-        tcaselect(tca_addr[i]);
-      }
-      if (!sensors_7700[i].begin()) {
-        Serial.print("ERROR ---- VEML "); Serial.print(names[i]); Serial.println(" not found");
-        neos[i]->colorWipe(255, 0, 0);
-        unsigned long then = millis();
-        while (millis() < then + 5000) {
-          Serial.print(".");
-          delay(100);
+        Serial.print("\nattempting to start lux sensor ");
+        Serial.println(names[i]);
+        if (tca_addr[i] > -1) {
+            tcaselect(tca_addr[i]);
         }
-        Serial.println();
-      }
-      else {
-        Serial.print("VEML "); Serial.print(names[i]); Serial.println(" found");
-        sensors_7700[i].setGain(gain); // talk about gain and integration time in the thesis
-        sensors_7700[i].setIntegrationTime(integration);// 800ms was default
-      }
+        if (!sensors_7700[i].begin()) {
+            Serial.print("ERROR ---- VEML "); Serial.print(names[i]); Serial.println(" not found");
+            neos[i]->colorWipe(255, 0, 0);
+            unsigned long then = millis();
+            while (millis() < then + 5000) {
+                Serial.print(".");
+                delay(100);
+            }
+            Serial.println();
+        }
+        else {
+            Serial.print("VEML "); Serial.print(names[i]); Serial.println(" found");
+            sensors_7700[i].setGain(gain); // talk about gain and integration time in the thesis
+            sensors_7700[i].setIntegrationTime(integration);// 800ms was default
+        }
     }
 }
 
@@ -213,21 +241,37 @@ void LuxManager::readLux() {
      * determining what the global lux is. 
      */
 
-    for (int i = 0; i < num_sensors; i++){
-        // read each sensor
-      if (tca_addr[i] > -1) {
-        tcaselect(tca_addr[i]);
-      }
-      double _t = sensors_7700[i].readLux();
-      if (_t > 1000000) {
-          _t = sensors_7700[i].readLux();
-          if (_t  < 1000000) {
-              lux[i] = _t;
-          }
-      } else {
-          lux[i] = _t;
-      }
+    if (num_7700_sensors > 0) {
+        for (int i = 0; i < num_7700_sensors; i++){
+            // read each sensor
+            if (tca_addr[i] > -1) {
+                tcaselect(tca_addr[i]);
+            }
+            double _t = sensors_7700[i].readLux();
+            if (_t > 1000000) {
+                _t = sensors_7700[i].readLux();
+                if (_t  < 1000000) {
+                    lux[i] = _t;
+                }
+            } else {
+                lux[i] = _t;
+            }
+        }
     }
+    if (num_6030_sensors > 0) {
+        for (int i = 0; i < num_6030_sensors; i++){
+            double _t = sensors_6030[i].readLight();
+            if (_t > 1000000) {
+                _t = sensors_6030[i].readLight();
+                if (_t  < 1000000) {
+                    lux[num_7700_sensors + i] = _t;
+                }
+            } else {
+                lux[num_7700_sensors + i] = _t;
+            }
+        }
+    }
+
     if (mode == TAKE_HIGHEST_LUX) {
         double highest = 0.0;
         for (int i = 0; i < num_sensors; i++) {
@@ -247,19 +291,19 @@ void LuxManager::readLux() {
     lux_total = lux_total + global_lux;
     lux_readings++;
     // now trigger an update to the brightness scaler
-      brightness_scaler = calculateBrightnessScaler();
-      num_brightness_scaler_vals++;
-      brightness_scaler_total += brightness_scaler;
-      // update all linked neogroups with the new brighness scaler
-      for (int i = 0; i < num_neo_groups; i++){
+    brightness_scaler = calculateBrightnessScaler();
+    num_brightness_scaler_vals++;
+    brightness_scaler_total += brightness_scaler;
+    // update all linked neogroups with the new brighness scaler
+    for (int i = 0; i < num_neo_groups; i++){
         neos[i]->setBrightnessScaler(brightness_scaler);
-      }
-      if (PRINT_BRIGHTNESS_SCALER_DEBUG == 0) {
-          dprint(PRINT_LUX_READINGS, "\tbs: "); 
-          dprintln(PRINT_LUX_READINGS, brightness_scaler);
-      };
-      updateMinMax();
-      last_reading = 0;
+    }
+    if (PRINT_BRIGHTNESS_SCALER_DEBUG == 0) {
+        dprint(PRINT_LUX_READINGS, "\tbs: "); 
+        dprintln(PRINT_LUX_READINGS, brightness_scaler);
+    };
+    updateMinMax();
+    last_reading = 0;
 }
 
 double LuxManager::calculateBrightnessScaler() {
@@ -329,77 +373,77 @@ void LuxManager::resetBrightnessScalerAvg() {
 
 void LuxManager::updateMinMax() {
     for (int i = 0; i < num_sensors; i++) {
-      if (lux[i] < min_reading && lux[i] > 0.0) {
-        min_reading = lux[i];
-      } else if (lux[i] > max_reading && lux[i] < 10000) {
-        max_reading = lux[i];
-      }
+        if (lux[i] < min_reading && lux[i] > 0.0) {
+            min_reading = lux[i];
+        } else if (lux[i] > max_reading && lux[i] < 10000) {
+            max_reading = lux[i];
+        }
     }
 }
 
 void LuxManager::resetMinMax() {
-  min_reading = 10000;
-  max_reading = 0;
+    min_reading = 10000;
+    max_reading = 0;
 }
 // todo move me to the correct place
 void LuxManager::calibrate(long len, bool first_time = true) {
-  // todo change this function so it takes the average of these readings
-  // TODO this is broken now that the manager looks after multiple sensors....
-  // 
-  printMinorDivide();
-  Serial.println("Starting Lux Calibration");
-  double lux_tot = 0.0;
-  for (int i = 0; i < 10; i++) {
-    delay(len / 10);
-    forceLuxReading(); // todo change this to not be hard coded
-    if (first_time) {
-      Serial.print(global_lux);
-      Serial.print("  ");
+    // todo change this function so it takes the average of these readings
+    // TODO this is broken now that the manager looks after multiple sensors....
+    // 
+    printMinorDivide();
+    Serial.println("Starting Lux Calibration");
+    double lux_tot = 0.0;
+    for (int i = 0; i < 10; i++) {
+        delay(len / 10);
+        forceLuxReading(); // todo change this to not be hard coded
+        if (first_time) {
+            Serial.print(global_lux);
+            Serial.print("  ");
+        }
+        lux_tot += global_lux;
+        // when we have the first 10 readings
     }
-    lux_tot += global_lux;
-    // when we have the first 10 readings
-  }
-  Serial.print("\nAverage lux readings : ");
-  global_lux = lux_tot / 10.0;
-  Serial.print(global_lux);
-  Serial.println();
-  if (first_time) {
-    lux_total = 0;
-    lux_readings = 0;
-  }
-  Serial.println("------------------------\n");
+    Serial.print("\nAverage lux readings : ");
+    global_lux = lux_tot / 10.0;
+    Serial.print(global_lux);
+    Serial.println();
+    if (first_time) {
+        lux_total = 0;
+        lux_readings = 0;
+    }
+    Serial.println("------------------------\n");
 }
 
 double LuxManager::forceLuxReading() {
-  readLux();
-  return global_lux;
+    readLux();
+    return global_lux;
 }
 
 bool LuxManager::update() {
     for (int i = 0; i < num_sensors; i++) {
-      if ((neos[i]->getLedsOn() == false && neos[i]->getOnOffLen() >= LUX_SHDN_LEN) || (neos[i]->getShdnLen() > LUX_SHDN_LEN)) {
-          // if currently in extreme lux shutdown then poll 20x faster
-        if (extreme_lux && last_reading > min_reading_time * 0.05) {
-            dprint(PRINT_LUX_DEBUG, "QUICK UPDATE due to extreme lux reading");
-            readLux();
-            if (neos[i]->getShdnLen() > LUX_SHDN_LEN) {
-              neos[i]->powerOn();
+        if ((neos[i]->getLedsOn() == false && neos[i]->getOnOffLen() >= LUX_SHDN_LEN) || (neos[i]->getShdnLen() > LUX_SHDN_LEN)) {
+            // if currently in extreme lux shutdown then poll 20x faster
+            if (extreme_lux && last_reading > min_reading_time * 0.05) {
+                dprint(PRINT_LUX_DEBUG, "QUICK UPDATE due to extreme lux reading");
+                readLux();
+                if (neos[i]->getShdnLen() > LUX_SHDN_LEN) {
+                    neos[i]->powerOn();
+                }
+                return true;
             }
-            return true;
-        }
-        else if (last_reading > min_reading_time) {
-            readLux();
-            if (neos[i]->getShdnLen() > LUX_SHDN_LEN) {
-              neos[i]->powerOn();
+            else if (last_reading > min_reading_time) {
+                readLux();
+                if (neos[i]->getShdnLen() > LUX_SHDN_LEN) {
+                    neos[i]->powerOn();
+                }
+                return true;
             }
-            return true;
+        } else if (last_reading > max_reading_time && neos[i]->getLedsOn() == true) {
+            // shdn len has to be longer to ensure the lux sensors get a good reading
+            neos[i]->shutdown(LUX_SHDN_LEN*1.25);
         }
-      } else if (last_reading > max_reading_time && neos[i]->getLedsOn() == true) {
-        // shdn len has to be longer to ensure the lux sensors get a good reading
-        neos[i]->shutdown(LUX_SHDN_LEN*1.25);
-      }
     }
-  return false;
+    return false;
 }
 
 #endif // __LUX_H__
